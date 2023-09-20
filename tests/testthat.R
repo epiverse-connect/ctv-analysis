f5d7a341a979d69e07a087db6d42be01561985de
James Baker <baker.james.jb@gmail.com>
2023-09-20 13:44:10 +0200

    first attempt at git api function

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(gh)
+
+test_check("cran-tesk-view-analysis")
---
+++ tests/testthat/test-gh.R
@@ -0,0 +1,13 @@
+# tests the functions in src/gh.R
+
+# load testthat package
+library(testthat)
+
+
+test_that("gh_file_exists", {
+  # Test that the function returns TRUE when the file exists
+  expect_true(gh_file_exists("README.md", "gh", "r-lib"))
+  # Test that the function returns FALSE when the file does not exist
+  expect_false(gh_file_exists("NOTREADME.md", "gh", "r-lib"))
+})
+```
